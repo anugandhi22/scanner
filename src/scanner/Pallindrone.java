6ae0dad248755064070585d9fcbc4c0e0d359851
anugandhi22 <anuradhagandhi2001@gmail.com>
2025-03-14 16:44:59 +0530

    first commit

--- src/scanner/Pallindrone.java
+++ src/scanner/Pallindrone.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Pallindrone {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a Number: ");
+		System.out.println("Enter a String: ");
 		int num=sc.nextInt();
 		int rev=0;
 		int n=num;
